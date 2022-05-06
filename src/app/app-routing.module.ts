9fc8134917f559a66496e6107eb770f9ec96d129
ancqit <ancqit@gmail.com>
2022-05-06 15:24:46 +0530

    created basc structure for the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
 import { TakeOrderComponent } from './components/take-order/take-order.component';
@@ -11,7 +12,7 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: OrderListComponent,
+    component: HomeComponent,
   },
   {
     path:'login',
