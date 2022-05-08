210556108dcbd3fbfab0f48d7519d11846c6be73
ancqit <ancqit@gmail.com>
2022-05-08 13:55:29 +0530

    added route for otp

--- src/app/components/verify-otp/verify-otp.component.ts
+++ src/app/components/verify-otp/verify-otp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify-otp',
@@ -7,19 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyOtpComponent implements OnInit {
   otp: any;
-  verifyOtp: boolean=false;
-  constructor() {}
+  verifyOtp: boolean = false;
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
-  onOtpChange(otpValue:any){
+  onOtpChange(otpValue: any) {
     console.log(otpValue);
-    this.otp=otpValue.toString();
-    if(this.otp.length == 6){
+    this.otp = otpValue.toString();
+    if (this.otp.length == 6) {
       this.verifyOtp = true;
-    } else  {
-      this.verifyOtp= false;
+    } else {
+      this.verifyOtp = false;
     }
-    
+  }
+
+  verifyOtpMode(){
+    this.router.navigate(['home']);
   }
 }
