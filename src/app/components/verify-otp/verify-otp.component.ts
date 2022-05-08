df56504c4927ddf6e1eaa3cc5f52de18c8b6e422
ancqit <ancqit@gmail.com>
2022-05-08 13:50:51 +0530

    added otp flow

--- src/app/components/verify-otp/verify-otp.component.ts
+++ src/app/components/verify-otp/verify-otp.component.ts
@@ -6,12 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verify-otp.component.scss'],
 })
 export class VerifyOtpComponent implements OnInit {
+  otp: any;
+  verifyOtp: boolean=false;
   constructor() {}
 
   ngOnInit(): void {}
 
   onOtpChange(otpValue:any){
     console.log(otpValue);
+    this.otp=otpValue.toString();
+    if(this.otp.length == 6){
+      this.verifyOtp = true;
+    } else  {
+      this.verifyOtp= false;
+    }
     
   }
 }
